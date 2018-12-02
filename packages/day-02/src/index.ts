4eba61222fdc32a74dbedc16b05fddc42e0d2806
Bart Callant <bart@callant.net>
2018-12-02 12:36:37 +0100

    day-02 challenge2

--- packages/day-02/src/index.ts
+++ packages/day-02/src/index.ts
@@ -159,3 +159,67 @@ console.log('Reuslt for testInputs: ', testInputsResult);
 
 const result = calculateChecksum(inputs);
 console.log('Reuslt for inputs: ', result); // Corrent anszer: 6000
+
+/**
+ * --- Part Two ---
+ *
+ * Confident that your list of box IDs is complete, you're ready to find the boxes full of prototype fabric.
+ *
+ * The boxes will have IDs which differ by exactly one character at the same position in both strings.
+ * For example, given the following box IDs:
+ *
+ * abcde
+ * fghij
+ * klmno
+ * pqrst
+ * fguij
+ * axcye
+ * wvxyz
+ * The IDs abcde and axcye are close, but they differ by two characters (the second and fourth).
+ * However, the IDs fghij and fguij differ by exactly one character, the third (h and u). Those must be the correct boxes.
+ *
+ * What letters are common between the two correct box IDs? (In the example above,
+ * this is found by removing the differing character from either ID, producing fgij.)
+ */
+
+function calculateCommonLetterParts(words: string[]): string {
+	for (const word of words) {
+		for (const w of words) {
+			if (w === word) {
+				continue;
+			}
+
+			let numberOfIncludes = 0;
+			let diffLetterIndex = -1;
+
+			for (let i = 0; i < w.length; i++) {
+				if (word[i] === w[i]) {
+					numberOfIncludes++;
+				} else {
+					diffLetterIndex = i;
+				}
+			}
+
+			if (numberOfIncludes >= (word.length - 1)) {
+				return Array.from(word).filter((_, i) => i !== diffLetterIndex).join('');
+			}
+		}
+	}
+
+	return '';
+}
+
+const testInputs2 = [
+	'abcde',
+	'fghij',
+	'klmno',
+	'pqrst',
+	'fguij',
+	'axcye',
+	'wvxyz',
+];
+const testInputsResult2 = calculateCommonLetterParts(testInputs2);
+console.log('Reuslt for testInputs2: ', testInputsResult2);
+
+const result2 = calculateCommonLetterParts(inputs);
+console.log('Reuslt2 for inputs: ', result2); // Correct answer: pbykrmjmizwhxlqnasfgtycdv
