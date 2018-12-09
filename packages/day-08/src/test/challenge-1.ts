802cdb4c14ffd9ccba95df38bf2a70355293f32f
Bart Callant <bart@callant.net>
2018-12-09 15:55:12 +0100

    day-08 challenge1

--- packages/day-08/src/test/challenge-1.ts
+++ packages/day-08/src/test/challenge-1.ts
@@ -5,9 +5,9 @@ import { getChallengeInput } from './fixtures/utils';
 import { challenge1 } from '../lib';
 
 test('test results', (t) => {
-	t.is(challenge1([]), 0);
+	t.is(challenge1('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'.split(' ')), 138);
 });
 
 test('results', async (t) => {
-	t.is(challenge1(getChallengeInput()), 0);
+	t.is(challenge1(getChallengeInput()), 36027);
 });
